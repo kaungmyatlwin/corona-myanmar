ecb41c37907478b412e5228ec2e36e9b824b1f94
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-03-31 14:05:33 +0630

    Remove Death ternary case

--- corona_myanmar.sh
+++ corona_myanmar.sh
@@ -2,6 +2,6 @@
   res=`curl -s https://corona-stats.online/MM\?source\=2 \
   | grep 'Myanmar' \
   | sed 's/\s*//g ; s/║//g ; s/│/; /g ; s/ //g ; s/\x1b\[[0-9;]*m//g' \
-  | awk -F';' '{print "😷: " $3 ","} {print "🤢: " $8 ","} {print $5!=" "?">    💀: " 0:$5}'`
+  | awk -F';' '{print "😷: " $3 ","} {print "🤢: " $8 ","} {print "💀: " $5}'`
 
 echo $res
