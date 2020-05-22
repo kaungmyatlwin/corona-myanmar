cf926ec02b3e79b749da90cb40c0e61a0fa2e114
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2020-05-22 15:38:30 +0630

    Show recovered count

--- corona_myanmar.sh
+++ corona_myanmar.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-  res=`curl -s https://corona-stats.online/MM\?source\=2 \
-  | grep 'Myanmar' \
-  | sed 's/\s*//g ; s/║//g ; s/│/; /g ; s/ //g ; s/\x1b\[[0-9;]*m//g' \
-  | awk -F';' '{print "😷: " $3 ","} {print "🤢: " $8 ","} {print "💀: " $5}'`
-
+#!/usr/bin/env bash 
+res=`curl -s https://corona-stats.online/MM\?source\=2 \ 
+  | grep 'Myanmar' \ 
+  | sed 's/\s*//g ; s/║//g ; s/│/; /g ; s/ //g ; s/\x1b\[[0-9;]*m//g' \ 
+  | awk -F';' '{print "😷: " $3 ","} {print "🤒:" $8 ","} {print "😊:" $7 ","}  {print "💀: " $5}'` 
+ 
 echo $res
